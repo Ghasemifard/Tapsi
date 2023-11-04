fdbfa33250ebb98d09dab7dca1a17345e9b7d33b
Ghasemifard <125352858+Ghasemifard@users.noreply.github.com>
2023-11-04 17:43:23 +0330

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click",function(){
+    navbar.classList.toggle("nav__expanded");
+});
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+    tab.addEventListener("click",(e)=>{
+        const targetTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+        // remove previous 'actived' classes
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+       //add new 'active' classes
+        tab.classList.add("active");
+        targetTabContent.classList.add("active");
+    });
+});
+
+
+
